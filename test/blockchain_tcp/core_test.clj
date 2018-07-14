a359c380861c9d13fd876a34536080b9544c0c91
Vu Tran <vutran@Vus-MacBook-Pro.local>
2018-07-14 11:13:50 +0700

    Init

---
+++ test/blockchain_tcp/core_test.clj
@@ -0,0 +1,7 @@
+(ns blockchain-tcp.core-test
+  (:require [clojure.test :refer :all]
+            [blockchain-tcp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
