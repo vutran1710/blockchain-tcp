9dff9a3e7d617dc919b4075d048c962dc0138b4c
vutran1710 <rhayaderv@gmailcom>
2018-07-21 18:57:19 +0700

    Return the chain on request

--- src/blockchain_tcp/core.clj
+++ src/blockchain_tcp/core.clj
@@ -1,98 +1,39 @@
 (ns blockchain-tcp.core
-  (:use [clojure.tools.cli])
-  (:gen-class)
-  (:require [manifold.deferred :as d]
-            [manifold.stream :as s]
-            [clojure.edn :as edn]
+  (:use [gloss.core]
+        [gloss.io]
+        [clojure.java.io]
+        [clojure.tools.cli])
+  (:require [manifold.stream :as s]
+            [manifold.deferred :as d]
             [aleph.tcp :as tcp]
-            [gloss.core :as gloss]
-            [gloss.io :as io]
-            [clojure.core.async
-             :as a
-             :refer [>! <! >!! <!! go chan buffer close! thread
-                     alts! alts!! timeout]]))
+            [blockchain-tcp.genesis :as bc]
+            [cheshire.core :refer [generate-string] :rename {generate-string jsonify}])
+  (:gen-class))
 
-;; See `two-way-handler`, `wrap-duplex-stream` and protocol is just convenient
-;; wrapper to use Clojure's EDN message format
+(def error-msg "Fuck off!\n")
 
-;; (def protocol
-;;   (gloss/compile-frame
-;;    (gloss/finite-frame :uint32
-;;                        (gloss/string :utf-8))
-;;    pr-str
-;;    edn/read-string))
+(defn routine-handler [s]
+  #(letfn [(resp [x] (s/put! s x) (s/close! s))]
+     (let [msg (String. %)]
+       (cond
+         (= "CHAIN\r\n" msg) (resp (jsonify @bc/chain))
+         :else (resp error-msg)))))
 
-;; (defn wrap-duplex-stream
-;;   [protocol s]
-;;   (let [out (s/stream)]
-;;     (s/connect
-;;      (s/map #(io/encode protocol %) out)
-;;      s)
-;;     (s/splice
-;;      out
-;;      (io/decode-stream s protocol))))
-
-;; (defn client
-;;   [host port]
-;;   (d/chain (tcp/client {:host host, :port port})
-;;            #(wrap-duplex-stream protocol %)))
-
-(defn -decode [bs]
-  "Receive []bytes, we can use any message format we want, some popular formats are:
-   - single line
-   - multiple-line-based, i.e: HTTP
-   - s-expressions
-   - json
-   - message-pack
-   - and of course, edn
-  "
-  (String. bs))
-
-(defn -encode [msg]
-  "Reverse of `-decode`"
-  (.getBytes msg))
-
-(defn one-way-handler [s info]
-  (println "--> new connection" info)
-  (s/connect (s/map (comp println -decode) s) s))
-
-(defn one-way-handler2 [s info]
-  (println "--> new connection" info)
-  ;; (s/consume (comp println -decode) s))
-  (s/consume #(println (-decode %)) s))
-
-(defn two-way-handler [s info]
-  (println "--> new connection" info)
-  (s/consume (fn [bs]
-               (println (-decode bs))
-               @(s/put! s (-encode (.concat "Reply: " (-decode bs)))))
-             s))
-
-(defn start-server
-  [handler port]
-  (tcp/start-server
-   ;; handler = (fn [stream info])
-   ;; we only need this if we use `wrap-duplex-stream` and/or protocol decoder
-   ;; (fn [s info] (handler s info))
-   handler
-   {:port port}))
+(defn master-handler [s info]
+  (s/consume (routine-handler s) s))
 
 (defn -main
   [& args]
   (let [[opts _ ban] (cli args
                           ["-p" "--port" "Port to listen to connections"
-                           :default 10000 :parse-fn #(Integer/parseInt %)]
+                           :default 10200 :parse-fn #(Integer/parseInt %)]
                           ["-h" "--help" "Show this help" :default false :flag true])]
     (when (:help opts)
-      (do
-        (println ban)
-        (System/exit 0)))
+      (println ban)
+      (System/exit 0))
     (try
-      (do
-        (println "Run on port: " (:port opts))
-        ;; (start-server one-way-handler (:port opts))
-        ;; (start-server one-way-handler2 (:port opts))
-        (start-server two-way-handler (:port opts))
-        )
+      (tcp/start-server master-handler {:port (:port opts)})
+      (println (format "Node is up on PORT %s." (:port opts)))
       (catch Exception e
-        (do (.printStackTrace e) (System/exit 0))))))
+        (do (.printStackTrace e)
+            (System/exit 0))))))
