6da3ccd67ee602627ed157bb8f8a1c2b207b95c6
vutran1710 <rhayaderv@gmailcom>
2018-07-22 17:18:04 +0700

    Add port-generator

--- src/blockchain_tcp/core.clj
+++ src/blockchain_tcp/core.clj
@@ -7,10 +7,26 @@
             [manifold.deferred :as d]
             [aleph.tcp :as tcp]
             [blockchain-tcp.genesis :as bc]
+            [utils :refer [generate-port]]
             [cheshire.core :refer [generate-string] :rename {generate-string jsonify}])
   (:gen-class))
 
 (def error-msg "Fuck off!\n")
+
+;; (defn start-up []
+;;   "Create a client, connect to any node in the existing network, then:"
+;;   "- Provide a local/stored chain, if there is"
+;;   "- Network resolve, return the valid chain"
+;;   "- Update to the latest chain"
+;;   "- Update the latest network-node"
+;;   "- Enter stake"
+;;   "- Mine, if desired"
+;;   (connect-to-server))
+;; (-> (connect-to-server)
+;;     (propose-local-chain)
+;;     (update-chain-and-nodes)
+;;     (request-stake)
+;;     (stand-by)))
 
 (defn routine-handler [s]
   #(letfn [(resp [x] (s/put! s x) (s/close! s))]
@@ -32,8 +48,9 @@
       (println ban)
       (System/exit 0))
     (try
-      (tcp/start-server master-handler {:port (:port opts)})
-      (println (format "Node is up on PORT %s." (:port opts)))
+      (let [port (generate-port)]
+        (tcp/start-server master-handler {:port port})
+        (println (format "Node is up on Port: %s." port)))
       (catch Exception e
         (do (.printStackTrace e)
             (System/exit 0))))))
--- src/utils.clj
+++ src/utils.clj
@@ -1,7 +1,46 @@
-(ns utils)
+(ns utils
+  (:require [clojure.java.io :as javis]))
 
+;; Consts
+(defonce TCP_DEFAULT_PORT 10200)
+(defn current-path [] (System/getProperty "user.dir"))
+(defn portfile [] (str (current-path) "/.ports"))
+
+
+;; Pure functions
 (defn now []
   (quot (System/currentTimeMillis) 1000))
 
 (defn get-values [a-map]
   (vec (map #(last %) (seq a-map))))
+
+(defn read-nth-line [file line-number]
+  (with-open [rdr (javis/reader file)]
+    (nth (line-seq rdr) (dec line-number))))
+
+(defn count-file-line [file]
+  (with-open [rdr (javis/reader file)]
+    (count (line-seq rdr))))
+
+(defn append-to-file [content file]
+  (spit file (str content "\n") :append true))
+
+(defn parse-port [port-string]
+  (try (read-string port-string)
+       (catch Exception e (println "Invalid port string!"))))
+
+
+;; Dirty functions // App functions
+(defn write-port-to-file-and-return-it [file port]
+  (do (append-to-file port file) port))
+
+(defn generate-port []
+  (let [file (portfile)]
+    (let [lc (count-file-line file)]
+      (if (> lc 0)
+        (->> (read-nth-line file lc)
+             parse-port
+             inc
+             (write-port-to-file-and-return-it file))
+        (->> TCP_DEFAULT_PORT
+             (write-port-to-file-and-return-it file))))))
