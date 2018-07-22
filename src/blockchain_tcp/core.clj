d02c152e2668251a876332a2c184c17c65a46eab
vutran1710 <rhayaderv@gmailcom>
2018-07-22 23:35:40 +0700

    Setting up client

--- src/blockchain_tcp/core.clj
+++ src/blockchain_tcp/core.clj
@@ -7,30 +7,28 @@
             [manifold.deferred :as d]
             [aleph.tcp :as tcp]
             [blockchain-tcp.genesis :as bc]
+            [blockchain-tcp.network :refer [connect-to-server]]
             [blockchain-tcp.utils :refer [generate-port]]
             [cheshire.core :refer [generate-string] :rename {generate-string jsonify}])
   (:gen-class))
 
 (def error-msg "Fuck off!\n")
 
-;; (defn start-up []
-;;   "Create a client, connect to any node in the existing network, then:"
-;;   "- Provide a local/stored chain, if there is"
-;;   "- Network resolve, return the valid chain"
-;;   "- Update to the latest chain"
-;;   "- Update the latest network-node"
-;;   "- Enter stake"
-;;   "- Mine, if desired"
-;;   (connect-to-server))
-;; (-> (connect-to-server)
-;;     (propose-local-chain)
-;;     (update-chain-and-nodes)
-;;     (request-stake)
-;;     (stand-by)))
+;; DEV ROADMAP:
+;; 1) Automate assignment of port everytime lein runs (auto-remove port from .port file too)
+;; - Create a client, connect to any node in the existing network, then:
+;; - Provide a local/stored chain, if there is
+;; - Network resolve, return the valid chain
+;; - Update to the latest chain
+;; - Update the latest network-node
+;; - Enter stake
+;; - Mine, if desired
+;; - Auto-remove port from .port when closing
 
 (defn routine-handler [s]
   #(letfn [(resp [x] (s/put! s x) (s/close! s))]
      (let [msg (String. %)]
+       (println msg)
        (cond
          (= "CHAIN\r\n" msg) (resp (jsonify @bc/chain))
          :else (resp error-msg)))))
@@ -49,6 +47,7 @@
       (System/exit 0))
     (try
       (let [port (generate-port)]
+        ;; (connect-to-server 10200)
         (tcp/start-server master-handler {:port port})
         (println (format "Node is up on Port: %s." port)))
       (catch Exception e
